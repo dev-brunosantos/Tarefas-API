dca4e7895e7a1bd892f43ae81f215432716649bd
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-30 13:51:15 -0300

    Criando os modelos de dados do DTO

--- src/tasks/dto/update-tarefa.dto.ts
+++ src/tasks/dto/update-tarefa.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTarefaDto } from './create-tarefa.dto';
 
-export class UpdateTarefaDto extends PartialType(CreateTarefaDto) {}
+export class UpdateTarefaDto extends PartialType(CreateTarefaDto) {
+    dtAtualizacao: Date;
+}
