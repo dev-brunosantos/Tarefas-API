7833745c4547d1c16e1e26271f98335ded2690ad
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-30 13:59:48 -0300

    Criando o Service das Tarefas

--- src/tasks/tarefas.service.ts
+++ src/tasks/tarefas.service.ts
@@ -1,19 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateTarefaDto } from './dto/create-tarefa.dto';
 import { UpdateTarefaDto } from './dto/update-tarefa.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class TarefasService {
-  create(createTarefaDto: CreateTarefaDto) {
-    return 'This action adds a new tarefa';
+
+  constructor(private prisma: PrismaService) {}
+
+  async Criar(createTarefaDto: CreateTarefaDto) {
+    const tarefaCadastrada = await this.prisma.tarefas.findFirst({ where: { tarefa: createTarefaDto.tarefa }})
+
+    if(!tarefaCadastrada) {
+      const novaTarefa = await this.prisma.tarefas.create({
+        data: {
+          tarefa: createTarefaDto.tarefa
+        }
+      })
+
+      throw new HttpException("Tarefa cadastrada com sucesso.", HttpStatus.CREATED)
+    }
+
+    throw new HttpException("Tarefa ja cadastrada.", HttpStatus.CONFLICT)
   }
 
-  findAll() {
-    return `This action returns all tarefas`;
-  }
+  async ListarTodos() {
+    const tarefas = await this.prisma.tarefas.findMany()
 
-  findOne(id: number) {
-    return `This action returns a #${id} tarefa`;
+    if(!tarefas) {
+      throw new HttpException("Não existe nenhuma tarefa cadastrada no sistema.", HttpStatus.NOT_FOUND)
+    }
+    
+    return tarefas
+  }
+  
+  async BuscarTarefa(id: string) {
+    const tarefaId = await this.prisma.tarefas.findFirst({ where: { id }})
+    
+    if(!tarefaId) {
+      throw new HttpException("Não existe nenhuma tarefa vinculado ao ID informado.", HttpStatus.NOT_FOUND)
+    }
+
+    return tarefaId
   }
 
   update(id: number, updateTarefaDto: UpdateTarefaDto) {
