2343463e55ae1bd98f5dcf024f0c3b2d8438c8f6
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-02 17:07:54 -0300

    Deixando o codigo mais limpo

--- dist/app/app.module.js
+++ dist/app/app.module.js
@@ -8,8 +8,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
 const tarefas_module_1 = require("../tasks/tarefas.module");
 let AppModule = class AppModule {
 };
@@ -17,8 +15,7 @@ exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [tarefas_module_1.TarefasModule],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        providers: [],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TarefasModule } from 'src/tasks/tarefas.module';
 
 @Module({
   imports: [TarefasModule],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
