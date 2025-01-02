ac22df363d8e8014e00ff133984c700dd06c8bf3
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-02 17:53:22 -0300

    Atualizações

--- dist/tasks/dto/create-tarefa.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateTarefaDto = void 0;
-class CreateTarefaDto {
-}
-exports.CreateTarefaDto = CreateTarefaDto;
-//# sourceMappingURL=create-tarefa.dto.js.map
--- dist/tasks/dto/update-tarefa.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { CreateTarefaDto } from './create-tarefa.dto';
-declare const UpdateTarefaDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateTarefaDto>>;
-export declare class UpdateTarefaDto extends UpdateTarefaDto_base {
-    dtAtualizacao: Date;
-}
-export {};
