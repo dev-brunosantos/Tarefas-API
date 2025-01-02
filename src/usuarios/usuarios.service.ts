1a167b9e45eef1366091915405ca4d538531bdc4
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-02 18:24:08 -0300

    Criando metodo de cadastro de usuário

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsuariosService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+
+  constructor(private prisma: PrismaService) { }
+
+  async Cadastrar(createUsuarioDto: CreateUsuarioDto) {
+    const usuarioExistente = await this.prisma.usuarios.findFirst({ where: { email: createUsuarioDto.email } })
+
+    if (!usuarioExistente) {
+      return await this.prisma.usuarios.create({
+        data: {
+          nome: createUsuarioDto.nome,
+          email: createUsuarioDto.email,
+          senha: createUsuarioDto.senha
+        }
+      })
+    }
+
+    throw new HttpException('Usuário já cadastrado', HttpStatus.BAD_REQUEST)
   }
 
   findAll() {
